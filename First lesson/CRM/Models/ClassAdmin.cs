dbe8729dfa91f4890bc91afcd7e67ec1e32b516f
Defstrong <sfayzulloev1@gmail.com>
2022-12-26 14:36:36 +0500

    aa

---
+++ First lesson/CRM/Models/ClassAdmin.cs
@@ -0,0 +1,35 @@
+﻿using First_lesson.CRM.DTO;
+using First_lesson.CRM.Services;
+
+namespace First_lesson.CRM.Models
+{
+    public class ClassAdmin
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public ClassAdmin(InputUserDto classInputUser, string role)
+        {
+            string date;
+            FirstName = classInputUser.FirstName;
+            LastName = classInputUser.LastName;
+            Patronymic = classInputUser.Patronymic;
+            DateOfBirth = classInputUser.DateOfBirth;
+            Age = ClassMethods.age(DateOfBirth);
+            Login = classInputUser.Login;
+            Password = classInputUser.Password;
+            Role = role;
+        }
+        public override string ToString()
+        {
+            return $"First Name: {FirstName}\nLast Name: {LastName}\nPatronymic:{Patronymic}\n" +
+                    $"Age: {Age} \nDate of Birth: {DateOfBirth.ToString("dd.MM.yyyy")}\n";
+        }
+    }
+}
---
+++ First lesson/CRM/Services/ModerServices.cs
@@ -0,0 +1,59 @@
+﻿using First_lesson.CRM.Models;
+using System;
+
+namespace First_lesson.CRM.Services
+{
+    public class ModerServices
+    {
+        static string inputTeg;
+        static List<User> requests = new List<User>();
+        static List<User> usersList = new List<User>();
+        static List<User> refuseRequests = new List<User>();
+        static List<int> indexes = new List<int>();
+        public static int countAcceptRequest = 0;
+        public static int countRefuseRequest = 0;
+        public static string Refuse;
+
+        public static void GeneralAccessForModer(ref List<User> Requests, ref List<User> UsersList, ref List<User> RefuseRequests)
+        {
+            requests = Requests;
+            usersList = UsersList;
+            refuseRequests = RefuseRequests;
+        }
+        public static void ModerLogin()
+        {
+            if (requests.Count == 0)
+                Console.WriteLine("\t\t\tNo requests\n\n");
+            else
+            {
+                Console.WriteLine("\t\t\tList of registration requests");
+                for (int i = 0; i < requests.Count; i++)
+                {
+                    Console.WriteLine($"\t\t{i + 1})");
+                    Console.WriteLine(requests[i].ToString());
+
+                    Console.WriteLine("\nAccept\t\tRefuse");
+                    inputTeg = Console.ReadLine();
+
+                    if (inputTeg == "Accept")
+                    {
+                        countAcceptRequest++;
+                        usersList.Add(requests[i]);
+                        indexes.Add(i);
+                    }
+                    else if (inputTeg == "Refuse")
+                    {
+                        countRefuseRequest++;
+                        Console.WriteLine("\t\t\tRejection reason\n");
+                        Refuse = Console.ReadLine();
+                        refuseRequests.Add(requests[i]);
+                        requests[i].UserStatus = Refuse;
+                        indexes.Add(i);
+                    }
+                }
+                for(int i = 0; i < requests.Count; i++)
+                    requests.Remove(requests[i]);
+            }
+        }
+    }
+}
